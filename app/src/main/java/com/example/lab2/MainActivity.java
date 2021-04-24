3b1a636196568ad021ea99da79bd9874389da967
Egle <kundrotaite.egle@gmail.com>
2021-04-24 20:48:52 +0300

    Initial commit

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.lab2;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
+import static com.example.lab2.utils.ElementsCalculator.getCharsCount;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner ddSelection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.ddSelection = (Spinner) findViewById(R.id.ddSelection);
+        ArrayList<String> selectionOptionsList = new ArrayList<>();
+        selectionOptionsList.add("Words");
+        selectionOptionsList.add("Chars");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, selectionOptionsList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.ddSelection.setAdapter(arrayAdapter);
+    }
+
+    public void btnCalculateOnClick(View view) {
+        EditText edUserInput = findViewById(R.id.edUserInput);
+        TextView tvOutput = findViewById(R.id.tvOutput);
+        String userInputText = edUserInput.getText().toString();
+
+        String selection = this.ddSelection.getSelectedItem().toString();
+        if(selection.equalsIgnoreCase("Chars")){
+            int count = getCharsCount(userInputText);
+            tvOutput.setText(String.valueOf(count));
+        }
     }
 }
